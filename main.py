20d6215004779e1bebcfd8c01f2c680edbf03083
restless-shadow <jcmanous@gmail.com>
2024-12-23 11:42:20 -0500

    added a few test runs to convert the file to a list with the split() function, covert to lower and convert to a dictionary object

--- main.py
+++ main.py
@@ -3,3 +3,14 @@
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
     print(file_contents)
+    print(len(file_contents.split()))
+
+    char_counts = {}
+    for char in file_contents.lower():
+        if char in char_counts:
+            char_counts[char] += 1
+        else:
+            char_counts[char] = 1
+    print(char_counts)
+
+
